98f90b0f9503e8708bf4044a3f162eeb36f1ead3
DavidStor <davidrusarm@gmail.com>
2018-07-26 19:38:46 -0700

    Implemented todo list and rendered it

--- reactApp/app.js
+++ reactApp/app.js
@@ -2,5 +2,25 @@ import React from 'react';
 import ReactDOM from'react-dom';
 
 
-ReactDOM.render(<p>React lives!</p>,
-    document.getElementById('root'));
+let dummyData = ['do dishes', 'clean my room', 'buy some bread', 'call your friend', 'take the dog out for a walk'];
+
+class TodoList extends React.Component { 
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (<ul>{dummyData.map((todo) => <Todo task={todo}/>)}</ul>);
+    }
+}
+
+class Todo extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (<li>{this.props.task}<button type="button">X</button></li>);
+    }
+}
+
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
