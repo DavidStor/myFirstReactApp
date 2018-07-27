f4c07be7711c386df7f858de47eb28be6285980c
DavidStor <davidrusarm@gmail.com>
2018-07-26 21:01:18 -0700

    rearranged flow of the  data prepared for future database integration , add css file

--- reactApp/app.js
+++ reactApp/app.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from'react-dom';
 
 
-let dummyData = [{ taskText: "do dishes", completed: false },{ taskText: "Have fun", completed: true }];
-
 class TodoList extends React.Component { 
     constructor(props){
         super(props);
     }
     render() {
-        return (<ul>{dummyData.map((todo) => <Todo task={todo.taskText} state={todo.completed}/>)}</ul>);
+        return (<ul>{this.props.todos.map((todo) => <Todo task={todo.taskText} state={todo.completed}/>)}</ul>);
     }
 }
 
@@ -18,8 +16,8 @@ class Todo extends React.Component {
         super(props);
     }
     render() {
-         return (<li><button type="button">X</button> {this.props.state? <strike>{this.props.task}</strike> : this.props.task }</li>);
-        //return (<li><button type="button">X</button>{this.props.task}</li>);
+         return (<li><button type="button">X</button> 
+            {this.props.state? <strike>{this.props.task}</strike> : this.props.task }</li>);
     }
 }
 
@@ -35,9 +33,17 @@ class InputLine extends React.Component {
 class TodoApp extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            todos: []
+        }
     }
+
+    componentDidMount() {
+        this.setState({todos: [{ taskText: "do dishes", completed: false },{ taskText: "Have fun", completed: true }] });
+    }
+
     render() {
-        return (<div><InputLine /><TodoList/></div>);
+        return (<div><InputLine/><TodoList todos={this.state.todos}/></div>);
     }
 }
 
